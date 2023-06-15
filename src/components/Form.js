9999c43d2de2c3680e333a4bd48c2f5c56e31e3b
= <=>
2023-06-15 17:02:22 +0900

    feat: Firebase Authentication 적용 진행

--- src/components/Form.js
+++ src/components/Form.js
@@ -17,6 +17,11 @@ const Form = ({ todoData, setTodoData }) => {
     e.preventDefault();
     // 새로운 todo 객체를 만들어준다.
     // 형식 즉, 키명을 구조를 지켜줌.
+    // 정규표현식 처리 예정
+    if (value === "") {
+      alert("내용을 입력하세요.");
+    }
+
     const newTodo = {
       id: Date.now(),
       title: value,
@@ -24,7 +29,17 @@ const Form = ({ todoData, setTodoData }) => {
     };
     // state 저장한다. 화면 리랜더링 된다.
     // todoData 에 추가.
-    setTodoData([...todoData, newTodo]);
+    // set함수 즉 setTodoData 에서
+    // 갱신 된 state 를 즉시 가지고 오기 위해서는
+    // set함수에 인자로
+    // 콜백함수를 전달한다.
+    // setTodoData([...todoData, newTodo]);
+    setTodoData(prev => {
+      return [...prev, newTodo];
+    });
+    // 로컬스토리지 저장
+    localStorage.setItem("fbTodoData", JSON.stringify([...todoData, newTodo]));
+    // axios post 호출 fbtodolist 추가하기
     // 입력창 초기화
     setValue("");
   };
